47f12fca93d9558f4df5435a548df1f0b0e37508
Eesa <eesaaamer5@gmail.com>
2020-04-23 00:37:58 -0400

    Retrieves and displays HTML data

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 URL = 'https://ca.finance.yahoo.com/world-indices'
-page_Data = requests.get(URL)
+page = requests.get(URL).text
+
+page_data = BeautifulSoup(page, "html.parser")
+print(page_data.prettify())
