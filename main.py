8679772c34365a4896fbb0a02e0a7c4cfc04f5ea
Eesa <eesaaamer5@gmail.com>
2020-04-24 02:00:23 -0400

    Added change in dollars to each stock

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ page_data = BeautifulSoup(page, "html.parser")
 stock_table = page_data.find('table').find("tbody").find_all('tr')
 
 for row in stock_table:
-    stock_data.append(list([row.find('td', attrs={'class': 'data-col1 Ta(start) Pend(10px)'}).text.strip(), row.find('td', attrs={'class': 'data-col2 Ta(end) Pstart(20px)'}).text.strip(), row.find('td', attrs={'class': 'data-col4 Ta(end) Pstart(20px)'}).text.strip()]))
+    stock_data.append(list([row.find('td', attrs={'class': 'data-col1 Ta(start) Pend(10px)'}).text.strip(), row.find('td', attrs={'class': 'data-col2 Ta(end) Pstart(20px)'}).text.strip(), row.find('td', attrs={'class': 'data-col3 Ta(end) Pstart(20px)'}).text.strip(),  row.find('td', attrs={'class': 'data-col4 Ta(end) Pstart(20px)'}).text.strip()]))
 
 print(stock_data)
