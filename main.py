159965f827a75f52393f22bf1bcdd35bc070a133
Eesa <eesaaamer5@gmail.com>
2020-04-24 00:44:07 -0400

    Extracts stock name and price for a single row

--- main.py
+++ main.py
@@ -5,4 +5,13 @@ URL = 'https://ca.finance.yahoo.com/world-indices'
 page = requests.get(URL).text
 
 page_data = BeautifulSoup(page, "html.parser")
-print(page_data.prettify())
+
+
+stock_name = page_data.find('td', attrs={'class': 'data-col1 Ta(start) Pend(10px)'})
+price_box = page_data.find('td', attrs={'class': 'data-col2 Ta(end) Pstart(20px)'})
+
+stock = stock_name.text.strip()
+price = price_box.text.strip()
+
+print(stock)
+print(price)
