c11111faf234a1ce979b9a71172091860fc5b640
Eesa <eesaaamer5@gmail.com>
2020-05-12 03:51:28 -0400

    Routed second page

--- stock-app/src/app/app-routing.module.ts
+++ stock-app/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { PortfolioComponent } from './portfolio';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PortfolioComponent },
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- stock-app/src/app/app.module.ts
+++ stock-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortfolioComponent } from './portfolio';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
