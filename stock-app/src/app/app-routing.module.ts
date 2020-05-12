b4b3d2fc22d58149ac27279f2f41eecca2d3f440
Eesa <eesaaamer5@gmail.com>
2020-05-12 19:54:57 -0400

    Routed home page as separate component

--- stock-app/src/app/app-routing.module.ts
+++ stock-app/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home';
 import { PortfolioComponent } from './portfolio';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'portfolio', component: PortfolioComponent },
+  
   // otherwise redirect to home
-  { path: '**', redirectTo: '' }
+  //{ path: '', redirectTo: '/home' }
 ];
 
 @NgModule({
