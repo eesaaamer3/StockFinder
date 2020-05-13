fd8be1377ba892597d78c2f7ad00a860d596e729
Eesa <eesaaamer5@gmail.com>
2020-05-12 23:35:12 -0400

    Fixed merging issue

--- stock-app/src/app/home/home.component.ts
+++ stock-app/src/app/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
 
-@Component({ templateUrl: 'home.component.html' })
+@Component({ 
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css'] 
+})
+
+
 export class HomeComponent {}
