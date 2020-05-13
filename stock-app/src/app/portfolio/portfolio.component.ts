cca0b525c53a717d29436b8248688716f2d06619
Eesa <eesaaamer5@gmail.com>
2020-05-12 23:41:20 -0400

    Added separate css for portfolio

--- stock-app/src/app/portfolio/portfolio.component.ts
+++ stock-app/src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
 
-@Component({ templateUrl: 'portfolio.component.html' })
+@Component({ 
+    templateUrl: './portfolio.component.html',
+    styleUrls: ['./portfolio.component.css'] 
+})
 export class PortfolioComponent {}
