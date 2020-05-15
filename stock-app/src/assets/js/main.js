1492e3c479489318d37ab2798bbc8e55cba94e6d
Eesa <eesaaamer5@gmail.com>
2020-05-15 00:13:44 -0400

    Active tab now working

--- stock-app/src/assets/js/main.js
+++ stock-app/src/assets/js/main.js
@@ -1,3 +1,15 @@
 function myTest() {
-    console.log('Welcome to custom js');
+    var btnContainer = document.getElementById("nav-list")
+
+    var btns = document.getElementsByClassName("btn")
+
+    for (var i = 0; i < btns.length; i++) {
+        console.log(i)
+        btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+        });
+    }
 }
+
