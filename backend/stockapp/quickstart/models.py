c9a572529b6bf6e45916002d883be171d4bbc738
Eesa <eesaaamer5@gmail.com>
2020-05-29 23:02:52 -0400

    Built database model

--- backend/stockapp/quickstart/models.py
+++ backend/stockapp/quickstart/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Stock(models.Model):
+    stock_name = models.CharField(max_length=50)
+    change_percent = models.CharField(max_length=50)
+    change_dollar = models.CharField(max_length=50)
+    stock_price = models.CharField(max_length=50)
+    
