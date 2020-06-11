5a3c4e9baa484b6f5d6b9e3d730559ef81f1b775
Eesa <eesaaamer5@gmail.com>
2020-06-10 23:12:23 -0400

    Edited model

--- backend/stockapp/quickstart/models.py
+++ backend/stockapp/quickstart/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Stock(models.Model):
-    stock_name = models.CharField(max_length=50)
+    stock_name = models.CharField(max_length=10)
     change_percent = models.CharField(max_length=5)
     change_dollar = models.CharField(max_length=5)
     stock_price = models.CharField(max_length=4)
