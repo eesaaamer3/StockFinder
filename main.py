50f049a8650bdb69025be3c41826718f99c50b4a
Eesa <eesaaamer5@gmail.com>
2020-06-15 23:46:47 -0400

    Deleted unnecessary lines

--- main.py
+++ main.py
@@ -12,5 +12,3 @@ stock_table = page_data.find('table').find("tbody").find_all('tr')
 
 for row in stock_table:
     stock_data.append(list([row.find('td', attrs={'class': 'data-col1 Ta(start) Pend(10px)'}).text.strip(), row.find('td', attrs={'class': 'data-col2 Ta(end) Pstart(20px)'}).text.strip(), row.find('td', attrs={'class': 'data-col3 Ta(end) Pstart(20px)'}).text.strip(),  row.find('td', attrs={'class': 'data-col4 Ta(end) Pstart(20px)'}).text.strip()]))
-
-print(stock_data)
