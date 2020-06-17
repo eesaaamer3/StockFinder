560815d67137af19b7f8040b579fd3365f27befe
Eesa <eesaaamer5@gmail.com>
2020-06-16 23:56:06 -0400

    .

--- backend/stockapp/quickstart/models.py
+++ backend/stockapp/quickstart/models.py
@@ -4,5 +4,5 @@ class Stock(models.Model):
     stock_name = models.CharField(max_length=10)
     change_percent = models.CharField(max_length=5)
     change_dollar = models.CharField(max_length=5)
-    stock_price = models.CharField(max_length=4)
+    stock_price = models.CharField(max_length=5)
     
